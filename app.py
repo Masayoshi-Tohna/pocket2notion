236cf4b19991ba568f1c85bf0acec11b226ab489
Masayoshi Tohna <nygmadm+github@gmail.com>
2025-06-01 18:11:38 +0900

    誤字脱字の修正

--- app.py
+++ app.py
@@ -254,7 +254,7 @@ if property_names:
     cols = st.columns(5)
     with cols[0]:
         choice_title = st.selectbox(
-            "CSV の「title」",
+            "Pocketのtitle」",
             options=["― 未選択 ―"] + property_names,
             key="map_title"
         )
@@ -262,7 +262,7 @@ if property_names:
 
     with cols[1]:
         choice_url = st.selectbox(
-            "CSV の「url」",
+            "Pocketの「url」",
             options=["― 未選択 ―"] + property_names,
             key="map_url"
         )
@@ -270,7 +270,7 @@ if property_names:
 
     with cols[2]:
         choice_time = st.selectbox(
-            "CSV の「time_added」",
+            "Pocket の「time_added」",
             options=["― 未選択 ―"] + property_names,
             key="map_time"
         )
@@ -278,7 +278,7 @@ if property_names:
 
     with cols[3]:
         choice_tags = st.selectbox(
-            "CSV の「tags」",
+            "Pocket の「tags」",
             options=["― 未選択 ―"] + property_names,
             key="map_tags"
         )
@@ -286,7 +286,7 @@ if property_names:
 
     with cols[4]:
         choice_status = st.selectbox(
-            "CSV の「status」",
+            "Pocket の「status」",
             options=["― 未選択 ―"] + property_names,
             key="map_status"
         )
